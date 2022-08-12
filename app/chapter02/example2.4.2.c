69225988528844e3c93c5d9fb48bb491efb997cb
Marian Domanski <mmdski@gmail.com>
2022-08-12 14:34:48 -0500

    add equation 2.4.1 and examples 2.4.1 and 2.4.2

---
+++ app/chapter02/example2.4.2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <spam.h>
+
+int
+main(void) {
+
+  // clang-format off
+  SPAMatrix a = NULL;
+  spa_mat_new_arr(&a, (double[]) {
+    1, 2, 2,
+    2, 5, 7,
+    3, 6, 6
+  }, 3, 3);
+  // clang-format on
+
+  puts("A=");
+  spa_mat_print(a);
+
+  spa_gauss_elim(a, spa_mat_prow_exch_zero);
+  puts("A reduced = ");
+  spa_mat_print(a);
+
+  SPAMatrixSize a_size       = spa_mat_size(a);
+  size_t        n_basic_cols = spa_gauss_basic_col_nos(a, NULL);
+
+  printf("n = %zu, rank(A) = %zu\n", a_size.n_cols, n_basic_cols);
+
+  spa_mat_free(&a);
+
+  return EXIT_SUCCESS;
+}
